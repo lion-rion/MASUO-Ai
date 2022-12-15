78a744c850719c86c6b2958e70b12528b7c14a10
lion-rion <lionact08@gmail.com>
2022-12-15 09:57:19 +0900

    🧹 modify

--- main.go
+++ main.go
@@ -9,16 +9,15 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-//192.168.0.105:20011
 func main() {
-	//conn, err := net.Dial("udp4", "192.168.0.104:20011")
-	conn, err := net.Dial("udp4", "192.168.0.105:20011")
+	/*   テストするロボットのipアドレスを入力   */
+	/*   192.168.0.10<ロボット番号>:20011   */
+	//conn, err := net.Dial("udp4", "127.0.0.1:20106") シュミレータ
+	conn, err := net.Dial("udp4", "127.0.0.1:20106")
 	if err != nil {
 		panic(err)
 	}
-	var i int = 0
-
-	var robotid uint32 = 3
+	var robotid uint32 = 0
 	var kickspeedx float32 = 0
 	var kickspeedz float32 = 0
 	var veltangent float32 = 0
@@ -27,11 +26,11 @@ func main() {
 	var spinner bool = false
 	var wheelsspeed bool = false
 
-	for {
-		robotid = 3
+	for i := 0; i < 300; i++ {
+		robotid = 3 //robot id
 		kickspeedx = 0
 		kickspeedz = 0
-		veltangent = 0.5
+		veltangent = 1
 		velnormal = 0
 		velangular = 0
 		spinner = false
@@ -59,9 +58,6 @@ func main() {
 		packet := &pb_gen.GrSim_Packet{
 			Commands: command,
 		}
-		// p := &pb.GrSim_Robot_Command{
-		// 	Id: 1.0,
-		// }
 		marshalpacket, _ := proto.Marshal(packet)
 		//println(marshalpacket)
 
@@ -71,55 +67,58 @@ func main() {
 			panic(err)
 		}
 		time.Sleep(3 * time.Millisecond)
-		i++
-		if i > 300 {
-			break
-		}
+
 	}
 
-	for {
-		robotid = 3
-		kickspeedx = 0
-		kickspeedz = 0
-		veltangent = 0
-		velnormal = 0
-		velangular = 0
-		spinner = false
-		wheelsspeed = false
+	//to stop
 
-		pe := &pb.GrSim_Robot_Command{
-			Id:          &robotid,
-			Kickspeedx:  &kickspeedx,
-			Kickspeedz:  &kickspeedz,
-			Veltangent:  &veltangent,
-			Velnormal:   &velnormal,
-			Velangular:  &velangular,
-			Spinner:     &spinner,
-			Wheelsspeed: &wheelsspeed,
-		}
+	for i := 0; i < 10; i++ {
 
-		var timestamp float64 = float64(time.Now().UnixNano() / 1e6)
-		var isteamyellow bool = false
+		var num uint32 = 0
+		for num = 0; num < 11; num++ {
+			robotid = num //robot id
+			kickspeedx = 0
+			kickspeedz = 0
+			veltangent = 0
+			velnormal = 0
+			velangular = 0
+			spinner = false
+			wheelsspeed = false
 
-		command := &pb.GrSim_Commands{
-			Timestamp:     &timestamp,
-			Isteamyellow:  &isteamyellow,
-			RobotCommands: []*pb.GrSim_Robot_Command{pe},
-		}
-		packet := &pb_gen.GrSim_Packet{
-			Commands: command,
-		}
-		// p := &pb.GrSim_Robot_Command{
-		// 	Id: 1.0,
-		// }
-		marshalpacket, _ := proto.Marshal(packet)
-		//println(marshalpacket)
+			pe := &pb.GrSim_Robot_Command{
+				Id:          &robotid,
+				Kickspeedx:  &kickspeedx,
+				Kickspeedz:  &kickspeedz,
+				Veltangent:  &veltangent,
+				Velnormal:   &velnormal,
+				Velangular:  &velangular,
+				Spinner:     &spinner,
+				Wheelsspeed: &wheelsspeed,
+			}
 
-		_, err = conn.Write([]byte(marshalpacket))
-		println("send : %v", marshalpacket)
-		if err != nil {
-			panic(err)
+			var timestamp float64 = float64(time.Now().UnixNano() / 1e6)
+			var isteamyellow bool = false
+
+			command := &pb.GrSim_Commands{
+				Timestamp:     &timestamp,
+				Isteamyellow:  &isteamyellow,
+				RobotCommands: []*pb.GrSim_Robot_Command{pe},
+			}
+			packet := &pb_gen.GrSim_Packet{
+				Commands: command,
+			}
+			// p := &pb.GrSim_Robot_Command{
+			// 	Id: 1.0,
+			// }
+			marshalpacket, _ := proto.Marshal(packet)
+			//println(marshalpacket)
+
+			_, err = conn.Write([]byte(marshalpacket))
+			println("send : %v", marshalpacket)
+			if err != nil {
+				panic(err)
+			}
+			time.Sleep(3 * time.Millisecond)
 		}
-		time.Sleep(3 * time.Millisecond)
 	}
 }
